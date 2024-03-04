a4f9899b9d858a66e5b1facefb01cdee1e5854e3
iieono <itstianmi@gmail.com>
2024-03-04 05:18:21 +0500

    first commit

--- birthday_reminder/settings.py
+++ birthday_reminder/settings.py
@@ -146,5 +146,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:8000/'
+    'http://127.0.0.1:8000/',
+    '*'
 ]
