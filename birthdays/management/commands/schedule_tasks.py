d5cb406cc2a2afe31fd690192ac701d7b42b0e53
iieono <itstianmi@gmail.com>
2024-03-04 14:26:39 +0500

    update

--- birthdays/management/commands/schedule_tasks.py
+++ birthdays/management/commands/schedule_tasks.py
@@ -11,20 +11,17 @@ class Command(BaseCommand):
         def job():
             while True:
                 schedule.run_pending()
-                time.sleep(1)  # Check every second
+                time.sleep(1)  
 
-        # Create a thread for running the scheduler loop
         scheduler_thread = threading.Thread(target=job)
-        scheduler_thread.daemon = True  # Daemonize the thread to stop it when the main thread exits
+        scheduler_thread.daemon = True  
         scheduler_thread.start()
 
-        # Schedule the task to run every 30 seconds
-        schedule.every(10).seconds.do(send_birthday_reminders)
-        # schedule.every().day.at("08:00").do(send_birthday_reminders)
+        schedule.every().day.at("08:00").do(send_birthday_reminders)
 
-        # Continue with runserver
+
         try:
             from django.core.management import call_command
             call_command('runserver', *args, **options)
         except KeyboardInterrupt:
-            pass  # Allow clean exit on KeyboardInterrupt
+            pass 
